909e3f1667886e629f897cdca28891dd19eaf930
Pavel Vinogradov <vino95@list.ru>
2016-04-18 16:58:10 +0300

    List example in the Lists file

--- MainDemo.Wpf/Domain/ListFieldsViewModel.cs
+++ MainDemo.Wpf/Domain/ListFieldsViewModel.cs
@@ -3,21 +3,52 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MaterialDesignColors.WpfExample.Domain
 {
+    public class TestClass
+    {
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public TestClass(string s)
+        {
+            Name = s;
+        }
+    }
+
     public class ListsAndGridsViewModel : INotifyPropertyChanged
     {
         private readonly ObservableCollection<SelectableViewModel> _items1;
         private readonly ObservableCollection<SelectableViewModel> _items2;
         private readonly ObservableCollection<SelectableViewModel> _items3;
         private bool? _isAllItems3Selected;
+        
 
         public ListsAndGridsViewModel()
         {
             _items1 = CreateData();
             _items2 = CreateData();
             _items3 = CreateData();
+            collectionA = new ObservableCollection<TestClass>();
+            collectionB = new ObservableCollection<TestClass>();
+            collectionA.Add(new TestClass("one"));
+            collectionA.Add(new TestClass("two"));
+            collectionA.Add(new TestClass("three"));
+            collectionB.Add(new TestClass("one"));
+            collectionB.Add(new TestClass("two"));
+            collectionB.Add(new TestClass("three"));
+            Click = new AnotherCommandImplementation(Clear);
         }
 
         public bool? IsAllItems3Selected
@@ -75,6 +106,10 @@ namespace MaterialDesignColors.WpfExample.Domain
             get { return _items1; }
         }
 
+        public ObservableCollection<TestClass> collectionA { get; set; }
+
+        public ObservableCollection<TestClass> collectionB { get; set; }
+
         public ObservableCollection<SelectableViewModel> Items2
         {
             get { return _items2; }
@@ -103,6 +138,17 @@ namespace MaterialDesignColors.WpfExample.Domain
                 yield return "Shake";
                 yield return "Lettuce";
             }            
+        }
+
+        public ICommand Click
+        {
+            get;set;
+        }
+
+        void Clear(object obj)
+        {
+            collectionA.Clear();
+            collectionB.Clear();
         }
     }
 }
